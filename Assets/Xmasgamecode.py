b299bcca6ac22601d39d5eb09f91726320d29f8a
Joaniplk <120081977+Joaniplk@users.noreply.github.com>
2023-03-25 23:16:49 +0200

    Fixing spaghetti code
    Split the code into modules and removed unnecessary code

--- Assets/Xmasgamecode.py
+++ Assets/Xmasgamecode.py
@@ -1,107 +1,62 @@
-import random
 import os
 import pygame
 import time
+import classes.drop as drop
+import classes.player as player
+import classes.text as txt
+
 pygame.font.init()
 WIDTH, HEIGHT = 1300, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Christmas Candy Collection Simulator 1.0")
-white = (255, 255, 255)
 D_YELLOW = (50, 50, 22)
 red = (255, 50, 50)
-green = (40, 255, 50)
-HITPOINTS_FONT = pygame.font.SysFont('Verdana', 40)  # NOQA
-SCORE_FONT = pygame.font.SysFont('Verdana', 40)
-END_FONT = pygame.font.SysFont('sans', 80)
 FPS = 60
-VEL, Co_VEL, Ca_VEL = 6.5, 5, 3
-PLAYERWIDTH, PLAYERHEIGHT, DROPWIDTH, DROPHEIGHT = 156, 256, 64, 64  # NOQA
-CANDY_IMAGE = pygame.image.load(os.path.join('candycanedrop.png'))  # NOQA
-CANDY = pygame.transform.scale(CANDY_IMAGE, (DROPWIDTH, DROPHEIGHT))
-PLAYER_CHARACTER_IMAGE = pygame.image.load(os.path.join('present character1.png'))
-PLAYER_CHARACTER = pygame.transform.scale(PLAYER_CHARACTER_IMAGE, (PLAYERWIDTH, PLAYERHEIGHT))
-COAL_IMAGE = pygame.image.load(os.path.join('coaldrop.png'))  # NOQA
-COAL = pygame.transform.scale(COAL_IMAGE, (DROPWIDTH, DROPHEIGHT))
+VEL, CO_VEL, CA_VEL = 6.5, 5, 3
+PLAYERWIDTH, PLAYERHEIGHT, DROPWIDTH, DROPHEIGHT = 156, 256, 64, 64
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('Xmas_background.png')), (WIDTH, HEIGHT))
+
+COAL = drop.Drop(DROPWIDTH, DROPHEIGHT, 0, 0, CO_VEL, 'coaldrop.png', WIDTH)
+CANDY = drop.Drop(DROPWIDTH, DROPHEIGHT, 0, 0, CA_VEL, 'candycanedrop.png', WIDTH)
+PLAYER = player.Player(PLAYERWIDTH, PLAYERHEIGHT, VEL, 'present character1.png', 5, 0, 'left', WIDTH)
+
+GAME_OVER_TXT = txt.Text('sans', 80, "You lost!", red)
+SCORE_TXT = txt.Text('Verdana', 40, "Your Score: " + str(PLAYER.score), D_YELLOW)
+HP_TXT = txt.Text('Verdana', 40, "HitPoints: " + str(PLAYER.HP), D_YELLOW)
 
 
 def main():
-    dropy = 0
-    candyx = round(random.randrange(300, WIDTH - 100))  # NOQA
-    coalx = round(random.randrange(300, WIDTH - 100))  # NOQA
-    last_direction = 'left'
-    game_over_text = END_FONT.render('You lost! ', True, red)
-    score = 0
-    hp = 100
-    character = pygame.Rect(WIDTH/2 - PLAYERWIDTH, 400, PLAYERWIDTH, PLAYERHEIGHT)
-    candy = pygame.Rect(candyx, dropy, DROPWIDTH, DROPHEIGHT)
-    coal = pygame.Rect(coalx, dropy, DROPWIDTH, DROPHEIGHT)
     clock = pygame.time.Clock()
     run = True
     while run:
         clock.tick(FPS)
+        # Closing the game with the 'X'
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # Closing the game with the 'X'
+            if event.type == pygame.QUIT:
                 run = False
-        if hp < 0:
-            end_text = 'You lost!'
-            WIN.blit(game_over_text, (WIDTH / 2 - game_over_text.get_width(), HEIGHT / 2 - game_over_text.get_height()))
+        if PLAYER.HP < 0:
+            GAME_OVER_TXT.draw(WIN, WIDTH / 2 - GAME_OVER_TXT.text.get_width() / 2, HEIGHT / 2 - GAME_OVER_TXT.text.get_height())
             pygame.display.update()
             time.sleep(1.75)
-            print(end_text)
             run = False
-        keys_pressed = pygame.key.get_pressed()    # Player movement
-        if keys_pressed[pygame.K_RIGHT] and character.x + VEL + PLAYERWIDTH < WIDTH + 80:
-            character.x += VEL
-            last_direction = 'right'
-        if keys_pressed[pygame.K_LEFT] and character.x - VEL > -77:
-            character.x -= VEL
-            last_direction = 'left'
-        if coal.colliderect(character):
-            hp -= 1
-        elif candy.colliderect(character):
-            score += 1
-            candy.y = 0
-            candy.x = round(random.randrange(300, WIDTH - 100))
-            coal.y = 0
-            coal.x = round(random.randrange(300, WIDTH - 100))
-        elif candy.y > HEIGHT:
-            hp -= 10
-        if last_direction == 'left':
-            hitpoints_text = HITPOINTS_FONT.render('HitPoints: ' + str(hp), True, D_YELLOW)  # NOQA
-            score_text = SCORE_FONT.render('Your Score: ' + str(score), True, D_YELLOW)
-            coal.y += Co_VEL
-            candy.y += Ca_VEL
-            f_character = pygame.transform.flip(PLAYER_CHARACTER, False, False)  # character rotation to match movement
-            WIN.blit(BACKGROUND, (0, 0))
-            WIN.blit(f_character, (character.x, character.y))
-            WIN.blit(CANDY, (candy.x, candy.y))
-            WIN.blit(COAL, (coal.x, coal.y))
-            WIN.blit(hitpoints_text, (WIDTH - hitpoints_text.get_width() - 10, 10))
-            WIN.blit(score_text, (0, 0))
-        pygame.display.update()
-        if coal.colliderect(character):
-            hp -= 1
-        if candy.colliderect(character):
-            score += 1
-            candy.y = 0
-            candy.x = round(random.randrange(300, WIDTH - 100))
-            coal.y = 0
-            coal.x = round(random.randrange(300, WIDTH - 100))
-        elif candy.y > HEIGHT:
-            hp -= 10
-        if last_direction == 'right':
-            hitpoints_text = HITPOINTS_FONT.render('HitPoints: ' + str(hp), True, D_YELLOW)  # NOQA
-            score_text = SCORE_FONT.render('Your Score: ' + str(score), True, D_YELLOW)
-            coal.y += Co_VEL
-            candy.y += Ca_VEL
-            f_character = pygame.transform.flip(PLAYER_CHARACTER, True, False)
-            WIN.blit(BACKGROUND, (0, 0))
-            WIN.blit(f_character, (character.x, character.y))
-            WIN.blit(CANDY, (candy.x, candy.y))
-            WIN.blit(COAL, (coal.x, coal.y))
-            WIN.blit(hitpoints_text, (WIDTH - hitpoints_text.get_width() - 10, 10))
-            WIN.blit(score_text, (0, 0))
+        PLAYER.move()
+        if  COAL.rect.colliderect(PLAYER.rect):
+            PLAYER.HP -= 1
+            COAL.reset_rect()
+        if CANDY.rect.colliderect(PLAYER.rect):
+            PLAYER.score += 1
+            CANDY.reset_rect()
+        if CANDY.rect.y > HEIGHT:
+            PLAYER.HP -= 1
+            CANDY.reset_rect()
+        if COAL.rect.y > HEIGHT:
+            COAL.reset_rect()
+        WIN.blit(BACKGROUND, (0, 0))
+        CANDY.update_drop(WIN)
+        COAL.update_drop(WIN)
+        PLAYER.draw(WIN)
+        HP_TXT.draw(WIN, WIDTH - HP_TXT.text.get_width() - 10, 10)
+        SCORE_TXT.draw(WIN, 0, 0)
         pygame.display.update()
 
 
