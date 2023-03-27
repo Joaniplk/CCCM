0bb203fec1e0d474112f54c7a370b36ea9513e86
Joaniplk <120081977+Joaniplk@users.noreply.github.com>
2023-03-27 07:13:46 +0300

    Update player.py
    Updated player.py to use the controls module

--- Assets/classes/player.py
+++ Assets/classes/player.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+import classes.controls as controls
 
 class Player:
     def __init__(self, width, height, vel, image_uri, hp, score, last_direction, bWIDTH):
@@ -11,6 +12,7 @@ class Player:
         self.HP = hp
         self.score = score
         self.last_direction = last_direction
+        self.controls = controls.Controls()
         PLAYER_CHARACTER_IMAGE = pygame.image.load(os.path.join(image_uri))
         self.PLAYER = pygame.transform.scale(PLAYER_CHARACTER_IMAGE, (self.width, self.height))
         self.x = self.bWIDTH/2 - self.width
@@ -22,21 +24,27 @@ class Player:
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
 
-    def move(self):
-        keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_RIGHT] and self.rect.x + self.VEL + self.width < self.bWIDTH + 80:
+    def __move(self):
+        self.controls.key_listen()
+        if self.controls.right and self.rect.x + self.VEL + self.width < self.bWIDTH + 80:
             self.rect.x += self.VEL
             self.last_direction = 'right'
-        if keys_pressed[pygame.K_LEFT] and self.rect.x - self.VEL > -77:
+        if self.controls.left and self.rect.x - self.VEL > -77:
             self.rect.x -= self.VEL
             self.last_direction = 'left'
+        
         self.x = self.rect.x
 
 
-    def draw(self, WIN):
+    def __draw(self, WIN):
         if self.last_direction == 'left':
             flip = False
         elif self.last_direction == 'right':
             flip = True
         f_character = pygame.transform.flip(self.PLAYER, flip, False)
         WIN.blit(f_character, (self.x, self.y))
+
+    
+    def update(self, WIN):
+        self.__move()
+        self.__draw(WIN)
